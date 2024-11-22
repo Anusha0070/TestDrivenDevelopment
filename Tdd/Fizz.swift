90c309d7569b06b1d84a7ee731bd781261c655a9
Anusha Raju <anusha.raju0070@gmail.com>
2024-11-22 12:31:36 -0800

    Created new file for Fizz

---
+++ Tdd/Fizz.swift
@@ -0,0 +1,22 @@
+//
+//  Fizz.swift
+//  Tdd
+//
+//  Created by Anusha Raju on 11/22/24.
+//
+
+
+struct Fizz{
+    
+    func printFizzBuzz(x: Int) -> String{
+        if(x % 3 == 0 && x % 5 == 0){
+            return "FizzBuzz"
+        } else if (x % 3 == 0) {
+            return "Fizz"
+        } else if (x % 5 == 0){
+            return "Buzz"
+        }
+        
+        return "Not Applicable"
+    }
+}
--- TddTests/TddTests.swift
+++ TddTests/TddTests.swift
@@ -25,20 +25,7 @@
 
 //1.for multiples of 3 print “Fizz” instead of the number and for the multiples of 5 print “Buzz“. For numbers which are multiples of both 3 and 5 print “FizzBuzz”.
 
-struct Fizz{
-    
-    func printFizzBuzz(x: Int) -> String{
-        if(x % 3 == 0 && x % 5 == 0){
-            return "FizzBuzz"
-        } else if (x % 3 == 0) {
-            return "Fizz"
-        } else if (x % 5 == 0){
-            return "Buzz"
-        }
-        
-        return "Not Applicable"
-    }
-}
+
 
 import XCTest
 @testable import Tdd
